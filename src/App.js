3d581b5b7dcf190c3bfa06a003d4b788e22d9bb0
Rachel DiCesare <racheldicesare93@gmail.com>
2020-02-11 07:58:42 -0500

    First commit and project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="virtual-desk">
+     <h2>My Virtual Desk</h2>
+     </div>
+    </div>
+  );
+}
+
+export default App;
